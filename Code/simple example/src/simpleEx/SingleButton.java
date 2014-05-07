4cec39383594293c432cd9f5405b00e3307c3b80
Håvard Tørresen <skabbkladden@gmail.com>
2014-05-07 11:10:22 +0200

    startet på enkelt eksempel

---
+++ Code/simple example/src/simpleEx/SingleButton.java
@@ -0,0 +1,12 @@
+package simpleEx;
+
+/**
+ * Simple  GUI-example with a single button and a listener
+ * @author Håvard Tørresen
+ *
+ */
+public class SingleButton {
+	public static void main(String[] args) {
+
+	}
+}
